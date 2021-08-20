2e88d10995ef58790bd0d8025af7a2a99b94ec71
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-08-20 19:50:17 -0300

    listView1 agora exibe lista de produtos

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -31,9 +31,9 @@ namespace Gerenciador_vitural_de_estoque
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -63,21 +63,25 @@ namespace Gerenciador_vitural_de_estoque
             // listView1
             // 
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(378, 541);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(179, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 47);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Editar produto";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(179, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 99);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Cadastrar produto";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -89,14 +93,14 @@ namespace Gerenciador_vitural_de_estoque
             this.button2.Text = "Remover produto";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // button1
             // 
-            this.button3.Location = new System.Drawing.Point(179, 71);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(173, 99);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Adicionar produto";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(179, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Editar produto";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
--- Form3.cs
+++ Form3.cs
@@ -12,12 +12,38 @@ namespace Gerenciador_vitural_de_estoque
 {
     public partial class Form3 : Form
     {
+        ConsultValues products = new ConsultValues("Table");
+        /*public int[] IdProduct;
+        public string[] namesAr;
+        public string[] quantidadeAr;
+        public string[] unidadeAr;
+        public string[] data_criacaoAr;*/
         public Form3()
         {
             InitializeComponent();
+            showAllProducts();
+        }
+
+        private void showAllProducts()
+        {
+            for(int i = 0; i < products.namesAr.Length; i++)
+            {
+                ListViewItem lv = new ListViewItem(products.namesAr[i]);
+                listView1.Items.Add(lv);
+            }
         }
 
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
