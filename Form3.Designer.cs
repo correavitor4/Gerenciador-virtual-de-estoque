6938a74fdfd2142defa88828695ce227d53ee0b8
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-08-24 13:07:08 -0300

    Preparando o formulário 3 para abrir o formulário de edição de produtos

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -101,6 +101,7 @@ namespace Gerenciador_vitural_de_estoque
             this.button1.TabIndex = 0;
             this.button1.Text = "Editar produto";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form3
             // 
--- Form3.cs
+++ Form3.cs
@@ -50,5 +50,10 @@ namespace Gerenciador_vitural_de_estoque
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
