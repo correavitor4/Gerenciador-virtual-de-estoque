915f5fc64f0a140af15804b467d560474b0df1b3
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-08-24 13:07:41 -0300

    Criação da classe que fará a edição do produto no banco de dados (ainda não testada)

---
+++ ClassEditProduct.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Gerenciador_vitural_de_estoque
+{
+    class ClassEditProduct
+    {
+
+        ConnectionDB conn = new ConnectionDB();
+        SqlCommand cmd = new SqlCommand();
+
+        private string message = "";
+        ClassEditProduct(string id,string name,string unity)
+        {
+
+            try
+            {
+                //Recebe o endereço do banco de dados
+                cmd.Connection = conn.connect();
+
+                //Comando que será executado
+                cmd.CommandText = string.Format("UPDATE Table SET nome_produto={0},unidade={1} WHERE id={2}",name,unity,id);
+
+                //executa o comando
+                cmd.ExecuteNonQuery();
+
+                message = "Edição efetudada com sucesso";
+                
+                conn.disconnect
+            
+            }
+            catch(SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                this.message = "Erro ao se conectar ao banco";
+            }
+
+        }
+
+        public string getMessage()
+        {
+            return this.message;
+        }
+    }
+}
