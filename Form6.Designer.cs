c66463e0bf5edb7603b9bd24a34e3e0c23de0b36
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-15 12:54:46 -0300

    Form 6 agora carrega itens vendidos na listView1

--- Form6.Designer.cs
+++ Form6.Designer.cs
@@ -30,17 +30,16 @@ namespace Gerenciador_vitural_de_estoque
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -69,42 +68,6 @@ namespace Gerenciador_vitural_de_estoque
             this.splitContainer1.SplitterDistance = 306;
             this.splitContainer1.TabIndex = 0;
             // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(884, 321);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "descrição";
-            this.columnHeader1.Width = 198;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "nome do cliente";
-            this.columnHeader2.Width = 260;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Data";
-            this.columnHeader3.Width = 103;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Produtos vendidos";
-            this.columnHeader4.Width = 219;
-            // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -125,15 +88,31 @@ namespace Gerenciador_vitural_de_estoque
             this.splitContainer2.SplitterDistance = 456;
             this.splitContainer2.TabIndex = 0;
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(51, 67);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(218, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Adicionar venda";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button3.Location = new System.Drawing.Point(305, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Pesquisar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(49, 114);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(358, 147);
+            this.listBox1.TabIndex = 2;
             // 
             // button2
             // 
@@ -144,31 +123,46 @@ namespace Gerenciador_vitural_de_estoque
             this.button2.Text = "Remover vendas";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // button1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(49, 114);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(358, 147);
-            this.listBox1.TabIndex = 2;
+            this.button1.Location = new System.Drawing.Point(51, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adicionar venda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // listView1
             // 
-            this.textBox1.Location = new System.Drawing.Point(49, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(233, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(884, 321);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
-            // button3
+            // columnHeader2
             // 
-            this.button3.Location = new System.Drawing.Point(305, 55);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Pesquisar";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.columnHeader2.Text = "nome do cliente";
+            this.columnHeader2.Width = 260;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Data";
+            this.columnHeader3.Width = 103;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Produtos vendidos";
+            this.columnHeader4.Width = 219;
             // 
             // Form6
             // 
@@ -196,7 +190,6 @@ namespace Gerenciador_vitural_de_estoque
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
--- Form6.cs
+++ Form6.cs
@@ -12,9 +12,38 @@ namespace Gerenciador_vitural_de_estoque
 {
     public partial class Form6 : Form
     {
+        ClassConsultSales sales = new ClassConsultSales();
         public Form6()
         {
+
             InitializeComponent();
+                
+            loadListViewItems();
+        }
+
+        private void loadListViewItems()
+        {
+            //remove itens que já estavam na listView, para recarregá-los
+            if (listView1.Items.Count > 0)
+            {
+                for(int i=listView1.Items.Count-1;i>=0; i--)
+                {
+                    listView1.Items.Remove(listView1.Items[i]);
+                }
+            }
+        
+            for(int i = 0; i < sales.productsSold.Length; i++)
+            {
+                string[] array = new string[3];
+                
+                array[0] = getClienteNameById(int.Parse(sales.clientId[i]));
+                array[1] = sales.data[i];
+                array[2] = sales.productsSold[i];
+
+                ListViewItem lv = new ListViewItem(array);
+                listView1.Items.Add(lv);
+
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,6 +58,24 @@ namespace Gerenciador_vitural_de_estoque
 
         }
 
-        
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string getClienteNameById(int id)
+        {
+            ConsultClients op = new ConsultClients();
+
+            for(int i = 0; i < op.names.Length; i++)
+            {
+                if(id == op.ids[i])
+                {
+                    return op.names[i];
+                }
+            }
+
+            return "";
+        }
     }
 }
