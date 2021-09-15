a3041059f8ac1f0d55f28bdf0560e391b32eb24d
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-14 23:48:00 -0300

    Atualizações necessárias da classe

--- ClassRegisterSale.cs
+++ ClassRegisterSale.cs
@@ -15,14 +15,14 @@ namespace Gerenciador_vitural_de_estoque
 
         private string message = "";
 
-        public ClassRegisterSale(string nomeCliente,string[] produtosVendidos,string[] produtosVendidosId,int idCliente)
+        public ClassRegisterSale(string nomeCliente,string produtosVendidos,string produtosVendidosId,string produtosVendidosQuantidade,string produtosVendidosUnidade,int idCliente)
         {
 
             try
             {
                 //RECEBE O ENDEREÇO DA CONEXÃO
                 cmd.Connection = conn.connect();
-                cmd.CommandText = string.Format("INSERT INTO [dbo].[Vendas] (nome_cliente,produtos_vendidos,produtos_vendidos_id,id_cliente) VALUES('{0}','{1}','{2}','{3}')", nomeCliente, produtosVendidos, produtosVendidosId, idCliente);
+                cmd.CommandText = string.Format("INSERT INTO [dbo].[Vendas] (nome_cliente,produtos_vendidos,produtos_vendidos_id,id_cliente,produtos_vendidos_quantidade,produtos_vendidos_unidade) VALUES('{0}','{1}','{2}','{3}','{4}','{5}')", nomeCliente, produtosVendidos, produtosVendidosId, idCliente,produtosVendidosQuantidade,produtosVendidosUnidade);
 
                 cmd.ExecuteNonQuery();
 
